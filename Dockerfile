bf420c2f5cfb2d7cc49f323cf8f8f5913b5c5a7e
Gavin <ray.gavin97@gmail.com>
2020-08-21 11:29:20 -0400

    Fix bash vs sh, add more logging

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM alpine:3.12
 
 COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
 RUN apk update && apk add --no-cache wget
 
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 # If a CLI version was given to Github Action inputs, then use that, otherwise default to latest release
-if [[ -z "$INPUT_HASURA_CLI_VERSION" ]]; then
+if [ -z "$INPUT_HASURA_CLI_VERSION" ]; then
+  echo "Hasura CLI version not provided, downloading latest release"
   hasura_cli_download_url="https://github.com/hasura/graphql-engine/releases/latest/download/cli-hasura-linux-amd64"
 else
+  echo "Downloading Hasura CLI $INPUT_HASURA_CLI_VERSION"
   hasura_cli_download_url="https://github.com/hasura/graphql-engine/releases/download/$INPUT_HASURA_CLI_VERSION/cli-hasura-linux-amd64"
 fi
 
@@ -13,14 +15,17 @@ wget --quiet --output-document /usr/local/bin/hasura "$hasura_cli_download_url" 
   exit 1
 }
 
+echo "Making Hasura CLI executable"
 # Make it executable
 chmod +x /usr/local/bin/hasura || {
   echo 'Failed making CLI executable'
   exit 1
 }
 
+echo "No path to Hasura project root given, using top-level repo directory"
 # CD into Hasura project root directory, if given and not current directory
-if [[ -n "$INPUT_PATH_TO_HASURA_PROJECT_ROOT" ]]; then
+if [ -n "$INPUT_PATH_TO_HASURA_PROJECT_ROOT" ]; then
+  echo "cd'ing to Hasura project root at $INPUT_PATH_TO_HASURA_PROJECT_ROOT"
   cd "$INPUT_PATH_TO_HASURA_PROJECT_ROOT" || {
     echo "Failed to cd into directory $INPUT_PATH_TO_HASURA_PROJECT_ROOT"
     exit 1
@@ -28,9 +33,10 @@ if [[ -n "$INPUT_PATH_TO_HASURA_PROJECT_ROOT" ]]; then
 fi
 
 # If migrations are enabled
-if [[ -n "$INPUT_HASURA_MIGRATIONS_ENABLED" ]]; then
+if [ -n "$INPUT_HASURA_MIGRATIONS_ENABLED" ]; then
+  echo "Running migrations"
   # If admin secret given in inputs, append it to migrate apply, else don't (use default from config.yaml)
-  if [[ -n "$INPUT_HASURA_ENDPOINT" ]]; then
+  if [ -n "$INPUT_HASURA_ENDPOINT" ]; then
     hasura migrate apply --endpoint "$INPUT_HASURA_ENDPOINT" --admin-secret "$INPUT_HASURA_ADMIN_SECRET" || {
       echo "Failed applying migrations"
       exit 1
@@ -46,23 +52,26 @@ else
 fi
 
 # If regression tests not enabled, end things here
-if [[ -z "$INPUT_HASURA_REGRESSION_TESTS_ENABLED" ]]; then
+if [ -z "$INPUT_HASURA_REGRESSION_TESTS_ENABLED" ]; then
   echo "Regression tests not enabled, finished."
   exit 0
 fi
 
+echo "Writing personal access token to config file"
 # Write Personal Access Token to config file in home directory for Pro CLI plugin
 echo "pat: $INPUT_HASURA_PERSONAL_ACCESS_TOKEN" >>~/.hasura/pro_config.yaml || {
   echo "Failed writing Pro personal access token to ~/.hasura/config.yaml"
   exit 1
 }
 
+echo "Installing Hasura CLI Pro plugin"
 # Install the Pro CLI plugin
 hasura plugins install pro || {
   echo "Failed installing Pro CLI plugin"
   exit 1
 }
 
+echo "Running regression tests"
 # Run regression tests
 hasura pro regression-tests run \
   --endpoint "$INPUT_HASURA_ENDPOINT" \
